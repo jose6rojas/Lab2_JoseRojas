84d7f5d8aa4207e23828c4fb2789a6affa8cee40
Jose Rojas <jose23rojas@unitec.edu>
2017-01-27 15:11:36 -0600

    Funcion del tercer ejercicio terminada.

--- lab2.cpp
+++ lab2.cpp
@@ -2,6 +2,7 @@
 // Laboratorio II - Programacion III
 
 #include <iostream>
+#include <cmath>
 
 using std::cin;
 using std::cout;
@@ -11,7 +12,7 @@ using std::endl;
 
 bool triangulos(int, int, int);
 
-double polinomios(int, int);
+double polinomios(int, double, int);
 
 int menu();
 
@@ -40,4 +41,37 @@ bool triangulos(int a, int b, int c)
 		return true;
 	else
 		return false;
+}
+
+double polinomios(int grado, double x, int version)
+{	
+	double respuesta;
+	if(version == 1)
+	{ // ascendente
+		int coeficientes[grado + 1];
+		for(int index = 0; index <= grado; index++)
+		{
+			cout << "Ingrese el valor del coeficiente " << index << ": ";
+			cin >> coeficientes[index];
+		}
+			
+		for(int index = 0; index <= grado; index++)
+		{
+			respuesta += coeficientes[index] *(pow(x, index));
+		}
+	} else // descendente
+	{
+		int coefs[grado + 1];
+		for(int index = grado; index >= 0; index--)
+		{
+			cout << "Ingrese el valor del coeficiente " << index << ": ";
+			cin >> coefs[index];
+		}
+				
+		for(int index = grado; index >= 0; index--)
+		{
+			respuesta += coefs[index] * (pow(x, index));
+		}
+	}
+return respuesta;
 }
