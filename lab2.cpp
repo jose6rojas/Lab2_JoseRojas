d6593a3788da2d968ea7c01514dd866aa8f79573
Jose Rojas <jose23rojas@unitec.edu>
2017-01-27 15:42:15 -0600

    Funcion del segundo ejercicio terminada.

--- lab2.cpp
+++ lab2.cpp
@@ -18,6 +18,51 @@ int menu();
 
 int main()
 {
+	int opcion = 0;
+	while(opcion != 4)
+	{
+		opcion = menu();
+		if(opcion == 4)
+			break;
+		if(opcion == 1)
+		{
+		}
+		if(opcion == 2)
+		{ // Formacion de Triangulos
+			int a, b, c;
+			cout << "Ingrese el valor del lado A: ";
+			cin >> a;
+			cout << "Ingrese el valor del lado B: ";
+			cin >> b;
+			cout << "Ingrese el valor del lado C: ";
+			cin >> c;
+			
+			bool formacion = triangulos(a, b, c);
+			if(formacion)
+				cout << "SI se puede formar un triangulo con estos lados." << endl;
+			else
+				cout << "NO se puede formar un triangulo con estos lados." << endl;
+		}
+		if(opcion == 3)
+		{ // Evaluacion de Polinomios
+			int version;
+			cout << "2. Evaluacion de Polinomios\n";
+			cout << "\t1. Ascendente\n";
+			cout << "\t2. Descendente\n";
+			cout << "Escoja una opcion: ";
+			cin >> version;
+
+			int grado;
+			cout << "Ingrese el grado del polinomio: ";
+			cin >> grado;
+			
+			double x;
+			cout << "Ingrese el valor de X: ";
+			cin >> x;
+
+			cout << "El resultado del polinomio es: " << polinomios(grado, x, version) << endl;
+		}		
+	}
 return 0;
 }
 
@@ -27,7 +72,8 @@ int menu()
 	cout << "Menu - Laboratorio II\n";
 		cout << "\t1. Ejercicio 1\n";
 		cout << "\t2. Creacion de Triangulos\n";
-		cout << "\t3. Evaluacion de Polinomios\n";
+		cout << "\t3. Formacion de Polinomios\n";
+		cout << "\t4. Salir\n";
 	cout << "Escoja una opcion: ";
 	cin >> opcion;
 return opcion;
